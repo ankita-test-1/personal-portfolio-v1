e567278157cebd48ad929f4917d6d890883faf1e
ankita-test-1 <ankita.rath.96@gmail.com>
2024-09-24 12:35:41 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Rahul Vikhe.</h2>;
+  const two = <h2 className="big-heading">Ankita Rath.</h2>;
   const three = <h3 className="big-heading">I build things on the Cloud.</h3>;
   const four = (
     <>
